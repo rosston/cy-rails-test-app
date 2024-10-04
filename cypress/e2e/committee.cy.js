b8832b5d9e39d17349d0490065531fd6092037e4
Erik P. Hansen <eph@erikphansen.com>
2024-10-04 09:01:41 -0700

    Make test one _always_ pass in interactive mode

--- cypress/e2e/committee.cy.js
+++ cypress/e2e/committee.cy.js
@@ -5,18 +5,30 @@ describe('Committee', () => {
 
   it('shows tickets', () => {
     cy.visit('/committees')
+
+    // Click on the first Committee. Its ID is unknown because of Fixtures 🤷‍♂️
     cy.get('a').first().click()
-    cy.get('li[id^="ticket-"]').should('have.length', 1)
+
+    // Click the New Ticket link
     cy.get('a').click()
-    cy.get('input[id="ticket_title"').type('New Ticket')
-    cy.get('textarea[id="ticket_body"').type('Some more text...')
+
+    // Enter data in the Ticket form
+    cy.get('input[id="ticket_title"').type('New ticket created by Cypress')
+    cy.get('textarea[id="ticket_body"').type(
+      'This ticket should be removed from the app between tests!',
+    )
     cy.get('input[type="submit"]').click()
-    cy.get('li[id^="ticket-"]').should('have.length', 2)
+
+    cy.contains('New ticket created by Cypress')
+    cy.contains('This ticket should be removed from the app between tests!')
   })
 
   it('resets tickets between tests', () => {
     cy.visit('/committees')
+
+    // Click on the first Committee. Its ID is unknown because of Fixtures 🤷‍♂️
     cy.get('a').first().click()
+
     cy.get('li[id^="ticket-"]').should('have.length', 1)
   })
 })
